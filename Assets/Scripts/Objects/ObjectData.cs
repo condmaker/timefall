7e856523446e381a0dddb62bb8fe07762c322eda
Miguel-Martinho <miguelpm2001@gmail.com>
2020-10-23 02:11:06 +0100

    Add UI elements functionality and start scriptableobjects

---
+++ Assets/Scripts/Objects/ObjectData.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CreateAssetMenu(fileName = "DataAsset", menuName = "DataAssets/Objects")]
+public class ObjectData : ScriptableObject
+{
+    [SerializeField]
+    private GameObject model;
+    [SerializeField]
+    private string     description;
+    [SerializeField]
+    private int        iD;
+    [SerializeField]
+    private Sprite     inventoryImage;
+
+}
--- Assets/Scripts/Player/EntityDetection.cs
+++ Assets/Scripts/Player/EntityDetection.cs
@@ -1,10 +1,10 @@
-﻿// Sync EntityDetection with PlayerInput after timers have been sorted
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
+
+// !!! Sync EntityDetection with PlayerInput after timers have been sorted
 
 public class EntityDetection : MonoBehaviour
 {
@@ -13,34 +13,47 @@ public class EntityDetection : MonoBehaviour
     // Variable that stores the GameObject collided with
     [SerializeField]
     private GameObject objectTouched;
+    private GameObject textDisplayObject;
+
+    private TextMeshProUGUI textToDisplay;
 
     // Bool that specifies is the player is colliding with an object
     private bool isColliding;
 
     void Start()
     {
-        pI = GetComponentInParent<PlayerInput>();
+        pI                = GetComponentInParent<PlayerInput>();
+        textDisplayObject = GameObject.Find("InteractionDisplay"); //!!! CHANGE THIS LATER !!!
+        textToDisplay     = textDisplayObject.GetComponent<TextMeshProUGUI>();
     }
 
     void FixedUpdate()
     {
-        //Debug.Log(pI.IsInteracting);
-        //Debug.Log(pI.IsWalking);
-        if (!pI.IsWalking && Input.GetKey("e") && isColliding )
+        if (!pI.IsWalking && Input.GetKey("e") && isColliding)
+        {
+            //if object has tag... (NPC, door/gate, object?)
+            textToDisplay.text = "You picked up a " + objectTouched.name;
             //get object ID call inventory function to store object ?
             Destroy(objectTouched);
+        }
     }
 
-    // This method is called when the EntityDetetction object collides with other triggers
+    // This method is called when the EntityDetection object collides with other triggers
     private void OnTriggerEnter(Collider other)
     {
         isColliding = true;
         objectTouched = other.gameObject;
+        textToDisplay.text = "Press 'E' to pick-up";
     }
     // This method is called when the EntityDetetction object stops colliding with other triggers
     private void OnTriggerExit(Collider other)
     {
         isColliding = false;
         objectTouched = null;
+        textToDisplay.text = " ";
+    }
+    private void CheckTag(GameObject objectTouched)
+    {
+        //if (objectTouched.tag == "gate") 
     }
 }
