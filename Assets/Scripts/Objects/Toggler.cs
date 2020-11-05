aa5bfab000f6d5037b63f688d142c24719568359
Miguel-Martinho <miguelpm2001@gmail.com>
2020-11-05 16:27:55 +0000

    Add working lever

---
+++ Assets/Scripts/Objects/Toggler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameobject1, gameobject2, gameobject3;
+    private Animator gameobject1Anim, gameobject2Anim, gameobject3Anim;
+
+    private void Awake()
+    {
+        gameobject1Anim = gameobject1.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+
+    }
+    public void Toggle()
+    {
+        if (gameobject1Anim.GetBool("OpenStatus") == true)
+            gameobject1Anim.SetBool("OpenStatus", false);
+        else
+            gameobject1Anim.SetBool("OpenStatus", true);
+
+
+    }
+}
