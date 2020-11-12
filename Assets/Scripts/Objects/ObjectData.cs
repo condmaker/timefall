9031e94a23858fdef91432ab78c6107eb8eeeb15
Marco Domingos <supersthotaku@gmail.com>
2020-11-12 14:54:01 +0000

    Remove get field from property, replace with a lambda at ObjectData

--- Assets/Scripts/Objects/ObjectData.cs
+++ Assets/Scripts/Objects/ObjectData.cs
@@ -4,13 +4,12 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-
-
 public abstract class ObjectData : ScriptableObject
 {
     [SerializeField]
     private InteractionType interactionType;
-    public InteractionType InteractionType { get => interactionType; }
+    public InteractionType  InteractionType => interactionType;
+
     [SerializeField]
     private string     description;
     [SerializeField]
