7662844f47a5ad7e5c2811c611bb0c9baeea6964
Marco Domingos <supersthotaku@gmail.com>
2020-12-16 12:33:57 +0000

    Add BrightnessSlider.cs on Scripts/UI/

---
+++ Assets/Scripts/UI/BrightnessSlider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BrightnessSlider : MonoBehaviour
+{
+
+    [SerializeField]
+    private float gammaCorrection;
+    private Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        gammaCorrection = slider.value;
+
+        RenderSettings.ambientLight = 
+            new Color(gammaCorrection, gammaCorrection, gammaCorrection, 1.0f);
+    }
+
+    
+}
