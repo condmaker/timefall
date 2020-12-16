abc464c8914326234c2d0f98e3d0efe79dc994e9
Daniel Fernandes <danielteotonio@sapo.pt>
2020-12-16 11:33:25 +0000

    Add InstatiateItem script to chest prefab

---
+++ Assets/Scripts/Objects/InstatiateItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstatiateItem : MonoBehaviour
+{
+    private ObjectStateHandler osh;
+
+    [SerializeField]
+    private Vector3 positionOffset;
+    [SerializeField]
+    private GameObject item;
+
+    public void Awake()
+    {
+        osh = GetComponent<ObjectStateHandler>();
+        if(osh != null)
+            osh.OnChangeState += Instatiate;
+    }
+
+    public void Instatiate(ObjectStateHandler oSH, short state)
+    {
+        Instantiate(item, transform.position + positionOffset, item.transform.rotation);
+    }
+}
