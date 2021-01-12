f12af1c6fe1f3358df53e7873da8135f18b4c366
Marco Domingos <supersthotaku@gmail.com>
2021-01-12 19:58:32 +0000

    Add multiple songs to the game, create MusicLoader.cs, organize folders

---
+++ Assets/Scripts/Managers/MusicLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoader : MonoBehaviour
+{
+    [SerializeField]
+    private SoundMng soundManager;
+
+    [SerializeField]
+    private AudioClip music;
+
+    [SerializeField]
+    private bool loop;
+
+    private void Awake()
+    {
+        soundManager?.PlayMusic(music, loop);
+    }
+
+}
