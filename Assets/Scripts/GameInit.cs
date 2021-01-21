f637828001b260e93a5c2675ab7f99851c3f049d
Daniel Fernandes <danielteotonio@sapo.pt>
2021-01-21 23:01:54 +0000

    Add options menu and Volume implementation

---
+++ Assets/Scripts/GameInit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        int checker = PlayerPrefs.GetInt("F_Check");
+        if (checker == 0)
+            SceneManager.LoadScene("MainMenu - Medieval");
+        else if(checker == 1)
+            SceneManager.LoadScene("MainMenu - Futuristic");
+
+    }
+}
---
+++ Assets/Scripts/UI/OptionsMenu/BrightnessSlider.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BrightnessSlider : MonoBehaviour
+{
+    private float gammaCorrection;
+    private Slider slider;
+    [SerializeField]
+    private Light PlayerLight;
+    private float minIntensity;
+    private float maxIntensity;
+    private float minRange;
+    private float maxRange;
+
+    private void Start()
+    {
+        minIntensity = 3;
+        maxIntensity = 3;
+        minRange = 10;
+        maxRange = 30;
+
+        slider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        //gammaCorrection = slider.value;
+        PlayerLight.intensity = 
+            (minIntensity + slider.value * (maxIntensity - minIntensity));
+        PlayerLight.range =
+            (minRange + slider.value * (maxRange - minRange));
+        RenderSettings.ambientLight = 
+            new Color(gammaCorrection, gammaCorrection, gammaCorrection, 1.0f);
+    }
+
+    
+}
