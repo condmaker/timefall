301e5d41ab994722ead3709c5b65f4df129d2f8a
Daniel Fernandes <danielteotonio@sapo.pt>
2021-01-21 23:33:26 +0000

    Add place Option menu UI elements

---
+++ Assets/Scripts/UI/SwitchMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menuToClose;
+    [SerializeField]
+    private GameObject menuToOpen;
+
+
+    public void SwitchMenus()
+    {
+        menuToClose.SetActive(false);
+        menuToOpen.SetActive(true);
+    }
+}
