724e22a55b5ff224d441dda3f76f5322c9503cb5
Marco Domingos <supersthotaku@gmail.com>
2021-01-23 22:11:12 +0000

    Add new sprites for sliders

---
+++ Assets/Scripts/UI/OptionEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class OptionEvent : MonoBehaviour
+{
+    public event Action OptionEnabled;
+    public event Action OptionDisabled;
+
+    private void OnEnable() => OnOptionEnabled();
+    private void OnDisable() => OnOptionDisabled();
+    private void OnOptionEnabled() => OptionEnabled();
+    private void OnOptionDisabled() => OptionDisabled();
+}
