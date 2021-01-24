681e17d34d75cf871021f2fcc3394744d45fd860
Marco Domingos <supersthotaku@gmail.com>
2021-01-24 22:04:27 +0000

    Fix canvas problems

--- Assets/Scripts/Player/Movement/PlayerInput.cs
+++ Assets/Scripts/Player/Movement/PlayerInput.cs
@@ -58,6 +58,7 @@ public class PlayerInput : MonoBehaviour
         eD = GetComponent<EntityDetection>();
 
         locke = false;
+        print(option);
         option.OptionDisabled += UnlockInput;
         option.OptionEnabled += LockInput;
     }
