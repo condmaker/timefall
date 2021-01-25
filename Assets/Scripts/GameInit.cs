bda8d1e7a13c490147d4545445e258f27bdbee7b
Bezerra <pabezerra96@gmail.com>
2021-01-25 17:19:04 +0000

    Doc-comment a few scripts

--- Assets/Scripts/GameInit.cs
+++ Assets/Scripts/GameInit.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Class responsible for initiating the correct Main Menu according to 
+/// player progression.
+/// </summary>
 public class GameInit : MonoBehaviour
 {
+    /// <summary>
+    /// Method that checks player progression and loads the respective Main 
+    /// Menu.
+    /// </summary>
     private void Awake()
     {
         int checker = PlayerPrefs.GetInt("F_Check");
