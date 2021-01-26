50623de77ce701ab2ad742ec1066f4ab53bdf800
Bezerra <pabezerra96@gmail.com>
2021-01-26 21:10:56 +0000

    Update teleport cheat

--- Assets/Scripts/Temporary/Teleporter.cs
+++ Assets/Scripts/Temporary/Teleporter.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     [SerializeField]
-    private GameObject ObjectToTeleport, teleportPoint1, teleportPoint2, teleportPoint3, teleportPoint4, teleportPoint5;
+    private GameObject ObjectToTeleport;
+    [SerializeField]
+    //private LinkedList<GameObject> TeleportPoints;
+    private GameObject[] TeleportPoints;
+
+    private int currentTpPoint;
+
     void Start()
     {
-        
+        currentTpPoint = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Y))
-            ObjectToTeleport.transform.position = teleportPoint1.transform.position;
-        if (Input.GetKeyDown(KeyCode.U))
-            ObjectToTeleport.transform.position = teleportPoint2.transform.position;
-        if (Input.GetKeyDown(KeyCode.I))
-            ObjectToTeleport.transform.position = teleportPoint3.transform.position;
-        if (Input.GetKeyDown(KeyCode.O))
-            ObjectToTeleport.transform.position = teleportPoint4.transform.position;
-        if (Input.GetKeyDown(KeyCode.P))
-            ObjectToTeleport.transform.position = teleportPoint5.transform.position;
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            PreviousPoint();
+            ObjectToTeleport.transform.position = 
+                TeleportPoints[currentTpPoint].transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            NextPoint();
+            ObjectToTeleport.transform.position = 
+                TeleportPoints[currentTpPoint].transform.position;
+        }
+
+        //ObjectToTeleport.transform.position = TeleportPoints[currentTpPoint].transform.position;
+    }
+
+    private void NextPoint()
+    {
+        currentTpPoint++;
+        if (currentTpPoint == TeleportPoints.Length)
+            currentTpPoint = 0;
+    }
+
+    private void PreviousPoint()
+    {
+        currentTpPoint--;
+        if (currentTpPoint < 0)
+            currentTpPoint = TeleportPoints.Length - 1;
     }
 }
