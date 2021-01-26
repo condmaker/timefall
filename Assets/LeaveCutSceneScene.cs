8ed8f7913feb439103c35dfaf7f418b18e524fb2
Daniel Fernandes <danielteotonio@sapo.pt>
2021-01-26 21:55:11 +0000

    Add Scene for the Custscenes

---
+++ Assets/LeaveCutSceneScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+/// <summary>
+/// Method responsible for leaving the cutscene Scene when the video
+/// has stop playing
+/// </summary>
+public class LeaveCutSceneScene : MonoBehaviour
+{
+    /// <summary>
+    /// The VideoPlayer component of the object
+    /// </summary>
+    private VideoPlayer videoPlayer;
+
+    /// <summary>
+    /// Method called when the scene starts
+    /// </summary>
+    public void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.loopPointReached += LoadNextScene;
+    }
+
+    private void LoadNextScene(VideoPlayer source)
+    {
+        SceneManager.LoadScene("First Stratum");
+    }
+}
