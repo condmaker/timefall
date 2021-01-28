c8540e2fa870c438acf23be3de4df4a38ec793ac
Marco Domingos <supersthotaku@gmail.com>
2021-01-28 00:31:18 +0000

    Make volume options affect intro scene volume

---
+++ Assets/Scripts/VideoVolume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+/// <summary>
+/// Automatically sets the current Video volume to the master volume.
+/// </summary>
+public class VideoVolume : MonoBehaviour
+{
+    /// <summary>
+    /// Video instance to change the audio volume.
+    /// </summary>
+    [SerializeField]
+    private VideoPlayer video;
+
+    private void Awake() =>
+        video.SetDirectAudioVolume(0,
+            (float) PlayerPrefs.GetInt("Master Volume") / 100);
+}
