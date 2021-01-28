fde3b647fd2fc3fcfa2043d0201440e34c3a705b
Marco Domingos <supersthotaku@gmail.com>
2021-01-28 17:37:11 +0000

    Add Screen showing full map on third stratum

---
+++ Assets/AspectChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectChange : MonoBehaviour
+{
+    Camera cam;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+        cam.aspect = 1.2f;
+    }
+
+}
