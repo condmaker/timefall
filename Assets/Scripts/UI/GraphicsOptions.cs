ab1fd5df389ee09235dca2a235e30650bc9a2fc9
Bezerra <pabezerra96@gmail.com>
2021-01-28 16:40:20 +0000

    Update graphics menu

--- Assets/Scripts/UI/GraphicsOptions.cs
+++ Assets/Scripts/UI/GraphicsOptions.cs
@@ -5,46 +5,110 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 
+/// <summary>
+/// Class responsible for controlling graphic options.
+/// </summary>
 public class GraphicsOptions : MonoBehaviour
 {
+    /// <summary>
+    /// Resolution Dropdown object.
+    /// </summary>
     [SerializeField]
     private TMP_Dropdown resolutionDropdown;
-    Resolution[] resolutions;
-    private int currentResolutionIndex;
+    
+    /// <summary>
+    /// Fullscreen Dropdown object
+    /// </summary>
+    [SerializeField]
+    private TMP_Dropdown fullScreenDropdown;
 
-    // Start is called before the first frame update
+    private Resolution[] resolutions;
+
+    /// <summary>
+    /// Called when the object is created. Sets the dropdown menus with all 
+    /// and the current selected options.
+    /// </summary>
     void Awake()
     {
+        SetResolutionsDropdown();
+        SetFullscreenDropdown();
+    }
+
+    /// <summary>
+    /// Select the current full screen mode into the drop down menu.
+    /// </summary>
+    private void SetFullscreenDropdown()
+    {
+        int currentModeIndex = 0;
+        FullScreenMode currentMode = Screen.fullScreenMode;
+        switch (currentMode)
+        {
+            case FullScreenMode.FullScreenWindow:
+                currentModeIndex = 0;
+                break;
+            
+            case FullScreenMode.ExclusiveFullScreen:
+                currentModeIndex = 1;
+                break;
+            
+            case FullScreenMode.Windowed:
+                currentModeIndex = 2;
+                break;
+        }
+
+        fullScreenDropdown.value = currentModeIndex;
+        fullScreenDropdown.RefreshShownValue();
+    }
+
+    /// <summary>
+    /// This method loads all the possible 
+    /// screen resolutions defined by Unity, and writes them into the 
+    /// ResolutionDropdown menu.
+    /// </summary>
+    private void SetResolutionsDropdown()
+    {
+        int currentResolutionIndex = 0;
         resolutions = Screen.resolutions.Select(resolution => new Resolution 
             { width = resolution.width, 
             height = resolution.height }).Distinct().ToArray();
 
         resolutionDropdown.ClearOptions();
 
-        List<string> resos = new List<string>();
+        List<string> resolutionOptions = new List<string>();
 
         foreach(Resolution r in resolutions)
         {
             string aux = r.width + " x " + r.height;
-            resos.Add(aux);
+            resolutionOptions.Add(aux);
 
-            if(r.Equals(Screen.currentResolution))
+            if(r.width == Screen.currentResolution.width &&
+                r.height == Screen.currentResolution.height)
             {
-                currentResolutionIndex = resos.Count - 1;
+                currentResolutionIndex = resolutionOptions.Count - 1;
             }
         }
 
-        resolutionDropdown.AddOptions(resos);
+        resolutionDropdown.AddOptions(resolutionOptions);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
     }
 
+    /// <summary>
+    /// Sets the selected resolution as current screen resolution.
+    /// </summary>
+    /// <param name="resolutionIndex">The selected resolution index in the 
+    /// dropdown menu.</param>
     public void SetResolution(int resolutionIndex)
     {
         Resolution res = resolutions[resolutionIndex];
         Screen.SetResolution(res.width, res.height, Screen.fullScreenMode);
     }
 
+    /// <summary>
+    /// Sets the selected FullScreenMode as the current Full Screen Mode.
+    /// </summary>
+    /// <param name="FullScreenIndex">The selected full screen mode index 
+    /// from the dropdown menu.</param>
     public void SetFullScreen(int FullScreenIndex)
     {
         FullScreenMode mode = Screen.fullScreenMode;
