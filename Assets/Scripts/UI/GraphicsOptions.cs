5b8283050bb273703723576d9c6923b53e3567b4
Bezerra <pabezerra96@gmail.com>
2021-01-28 01:47:58 +0000

    Update graphic options menu

---
+++ Assets/Scripts/UI/GraphicsOptions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsOptions : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    private int currentResolutionIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> resos = new List<string>();
+
+        foreach(Resolution r in resolutions)
+        {
+            string aux = r.width + " x " + r.height;
+            resos.Add(aux);
+
+            if(r.Equals(Screen.currentResolution))
+            {
+                currentResolutionIndex = resos.Count - 1;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resos);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreenMode);
+    }
+
+    public void SetFullScreen(int FullScreenIndex)
+    {
+        FullScreenMode mode = Screen.fullScreenMode;
+        switch (FullScreenIndex)
+        {
+            case 0:
+                mode = FullScreenMode.FullScreenWindow;
+                break;
+            case 1:
+                mode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 2:
+                mode = FullScreenMode.Windowed;
+                break;
+        }
+        Screen.fullScreenMode = mode;
+    }
+}
